2cdc539612742f6a23de32d4e01ee7d926496493
AymenHa01 <hattabaymen15@gmail.com>
2025-08-05 12:24:14 -0700

    change prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   groqApiKey: 'your-production-api-key-here', // Remplacez par votre clé API de production
-  apiUrl: 'https://your-production-api-url.com', // Remplacez par votre URL d'API de production
-  websocketUrl: 'wss://your-production-websocket-url.com/ws' // Remplacez par votre URL WebSocket de production
+  apiUrl: 'https://artba-fyc3dkejbpdkgxez.westeurope-01.azurewebsites.net', // Remplacez par votre URL d'API de production
+  websocketUrl: 'https://artba-fyc3dkejbpdkgxez.westeurope-01.azurewebsites.net/ws' // Remplacez par votre URL WebSocket de production
 };
